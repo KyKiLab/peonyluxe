99cd05de0cc0235ad1765049303212feed8868ad
Indira2406 <59651069+Indira2406@users.noreply.github.com>
2021-08-17 10:42:34 +0600

    conflict resolved

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,5 +2,5 @@ import "%modules%/header/header";
 import "%modules%/footer/footer";
 import "%modules%/blog/slider";
 import "%modules%/kindwords/slider";
+import "%modules%/home/home-banner";
 import "%modules%/meet-us/meet-us";
-

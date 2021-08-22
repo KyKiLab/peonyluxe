11d0b0651da3a020fa097ac9ac37d8bdd750e7e5
maralov <aralov.mykhailo@gmail.com>
2021-08-22 23:36:09 +0300

    fix popup

--- src/blocks/modules/popup/popup.js
+++ src/blocks/modules/popup/popup.js
@@ -1,16 +1,18 @@
 jQuery(function ($) {
   $(function () {
     const wpcf7Elm = document.querySelector(".wpcf7");
-    const popup = $("#popup");
+    const popup = $(".overlay");
+
     wpcf7Elm &&
       wpcf7Elm.addEventListener(
         "wpcf7submit",
         function () {
           popup.show();
-
+          document.body.style.overflow = "hidden";
           setTimeout(() => {
             popup.hide();
-          }, 2000);
+            document.body.style.overflow = "";
+          }, 3000);
         },
         false
       );

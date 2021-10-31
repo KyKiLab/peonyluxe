232fbb9587579d2174c3b0f908bfcce195c0c3a7
maralov <aralov.mykhailo@gmail.com>
2021-10-31 19:35:05 +0200

    two-columns-text-block

--- src/blocks/components/read-more/read-more.js
+++ src/blocks/components/read-more/read-more.js
@@ -1,21 +1,18 @@
-const readMoreBtns = document.querySelectorAll(".js-read_more-btn");
+import $ from "jquery";
 
-readMoreBtns.length
-  ? readMoreBtns.forEach((readMoreBtn) => {
-      readMoreBtn.addEventListener("click", changeClass);
-    })
-  : null;
+jQuery(function ($) {
+  $(function () {
+    const $readMoreBtns = $(".js-read-more-btn");
 
-function changeClass(e) {
-  const content = e.target
-    .closest(".js-read_more-block")
-    .querySelector(".js-read_more-content");
+    $readMoreBtns.on("click", function (e) {
+      const content = $(this).closest(".js-read-more-block");
+      content.toggleClass("two-columns-text-block--show");
 
-  content.classList.toggle("show");
-
-  if (content.classList.contains("show")) {
-    e.target.innerHTML = "Read Less";
-  } else {
-    e.target.innerHTML = "Read More";
-  }
-}
+      if (content.hasClass("two-columns-text-block--show")) {
+        $(this).text("Read Less");
+      } else {
+        $(this).text("Read More");
+      }
+    });
+  });
+});

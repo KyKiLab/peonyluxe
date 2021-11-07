fe7dcd0a87ba83565441479265d58d7c4a25de11
maralov <aralov.mykhailo@gmail.com>
2021-11-07 15:52:20 +0200

    fix readmore

--- src/blocks/components/read-more/read-more.js
+++ src/blocks/components/read-more/read-more.js
@@ -16,3 +16,25 @@ jQuery(function ($) {
     });
   });
 });
+
+const readMoreBtns = document.querySelectorAll(".js-read_more-btn");
+
+ readMoreBtns.length
+   ? readMoreBtns.forEach((readMoreBtn) => {
+       readMoreBtn.addEventListener("click", changeClass);
+     })
+   : null;
+
+ function changeClass(e) {
+   const content = e.target
+     .closest(".js-read_more-block")
+     .querySelector(".js-read_more-content");
+
+   content.classList.toggle("show");
+
+   if (content.classList.contains("show")) {
+     e.target.innerHTML = "Read Less";
+   } else {
+     e.target.innerHTML = "Read More";
+   }
+ }
